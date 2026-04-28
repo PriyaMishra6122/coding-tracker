69645aa72a98ae2a0bd6b54bf3a6ca7bd047a69f
PriyaMishra6122 <priyamishra6111@gmail.com>
2026-04-28 12:01:44 +0530

    Add files via upload
    initial upload

---
+++ code tracer/script.js
@@ -0,0 +1,24 @@
+function addTask() {
+  let input = document.getElementById("taskInput");
+  let task = input.value;
+
+  if (task === "") return;
+
+  let li = document.createElement("li");
+  li.innerText = task;
+
+  li.onclick = function () {
+    li.style.textDecoration = "line-through";
+  };
+
+  let btn = document.createElement("button");
+  btn.innerText = "Delete";
+  btn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(btn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
